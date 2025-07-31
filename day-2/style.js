879f127a9d67ae62a3b16bf8e09078f1138c8b63
saraswati1664 <saraswati74888@gmail.com>
2025-07-31 22:14:42 +0530

    js

--- day-2/style.js
+++ day-2/style.js
@@ -49,3 +49,29 @@ console.log(ans);
 //  bitwise XOR                                           (query h........)
 let tryans = num_1^num_2;
 console.log(tryans);// 7 answer
+
+
+//bitwise operators...............................
+                                               //TASKS..
+//The postfix and prefix forms (++ , --).
+
+let number1 = 1, number2 = 1;
+
+let number3 = ++number1; // 2
+let number4 = number2++; // 1
+console.log(number3);
+console.log(number4);
+
+//Assignment result
+let sara = 2;
+
+let sara2 = 1 + (sara *= 2);
+console.log(sara2); //5
+
+console.log(" \t \n" - 2);  // 14 questions try.
+
+//tried on live server
+let x = prompt("First number?", 1);
+let y = prompt("Second number?", 2);
+
+alert(+x + +y); // should be three default not 12.
