96c39c35ae0719dd57f5315accb087b0e69f36c4
saraswati1664 <saraswati74888@gmail.com>
2025-07-31 23:55:41 +0530

    jsprt2

---
+++ day-2..Part-2/style.js
@@ -0,0 +1,35 @@
+                          // --------------------------Comparisons----------------------
+ //  bitwise XOR
+ //  null vs 0  
+ //  undeined vs 0    
+ //logical oprators ke alert ke alert wale task  
+ 
+                           // --------------------------TASKS----------------------
+console.log(5 > 4)    //yes
+console.log("apple" > "pineapple")  //false
+console.log("2" > "12") //true
+console.log(undefined == null)  //true
+console.log(undefined === null)  //false
+console.log(null == "\n0\n")  //false
+console.log(null === +"\n0\n") //false   
+
+                           // --------------------------Conditional branching----------------------
+if ("0") {
+  console.log( 'Hello' );
+}
+// const offical_name =  "ECMAScript";
+// let result = prompt("What is the “official” name of JavaScript?");
+// if(result == offical_name){
+//     console.log("Right!");
+// }
+// else{
+//     console.log("You don't know? “ECMAScript”!");
+// }
+
+//ternary oprator
+// let result = (a + b < 4) ? 'below' : 'over' ;
+
+// let text =(login == 'Employee')? 'Hello':(login == 'Director')? 'Greetings':(login == '')? 'No login':'';
+
+//logical oprators
+alert(alert(1) && alert(2) );
