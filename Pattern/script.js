9c921f8d98655bcce09c44d0d59ffc9e41d57f4a
saraswati1664 <saraswati74888@gmail.com>
2025-08-05 22:42:29 +0530

    patterns

--- Pattern/script.js
+++ Pattern/script.js
@@ -46,7 +46,15 @@ for(let i = 1;i<=num;i++){
     }
     console.log();
  }
-
+ console.log();
+//pattern triangle 4*4
+ let n = 4;
+ for(let i = 0; i<n;i++){
+    for(let j = 0;j<i+1;j++){
+        process.stdout.write(i + 1 + " ");
+    }
+    console.log();
+ }
 
 
 
